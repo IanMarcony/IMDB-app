a1bd70a4c0b988ca9f845a20d18b615d47040db9
IanMarcony <imarconyls@gmail.com>
2021-01-21 17:04:15 -0400

    Created Folders Projects

---
+++ mobile/src/navigation/NavigationApp.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+
+import Login from "../views/Login"
+import Home from "../views/Home"
+import Register from "../views/Register"
+
+const Stack = createStackNavigator()
+
+const NavigationApp = () => {
+  return( < NavigationContainer>
+    <Stack.Navigator initialRouteName="Login"  >
+      <Stack.Screen name="Login"
+                    component={Login}
+                    screenOptions={{headerShown:false}} />
+      <Stack.Screen name="Register"
+                    component={Register}
+                    screenOptions={{headerShown:false}}/>
+      <Stack.Screen name="Home" component={Home}/>
+    </Stack.Navigator>
+  </ NavigationContainer>);
+}
+
+export default NavigationApp;
