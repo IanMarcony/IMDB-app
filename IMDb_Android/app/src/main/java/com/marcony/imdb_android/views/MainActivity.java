74ba7008c45beade4a8b886c7a4b14582b20dbad
IanMarcony <imarconyls@gmail.com>
2021-01-26 16:00:46 -0400

    Added Project in Android Studio

---
+++ IMDb_Android/app/src/main/java/com/marcony/imdb_android/views/MainActivity.java
@@ -0,0 +1,27 @@
+package com.marcony.imdb_android.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.marcony.imdb_android.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
